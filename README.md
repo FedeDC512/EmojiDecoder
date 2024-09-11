367e8ae20b0b3abd2b298e10b8cb1912b633c665
Federico Agnello <78120567+FedeDC512@users.noreply.github.com>
2024-09-11 22:46:26 +0200

    Refactor functions and result display, and add working random emoji button

--- README.md
+++ README.md
@@ -1,13 +1,36 @@
 # EmojiDecoder
 
 ### To Do:
-- [ ] Explore emoji-regex
 - [ ] Fix the isEmoji check
+- [ ] Add Footer (?)
 - [x] Fix Header (add links)
 - [x] Fix Background (theme?)
-- [ ] Add Footer (?)
-- [ ] Add page name and icon
+- [x] Add page name and icon
+- [ ] Made the info table width static
+- [ ] Add emoji decription in the info table
+- [ ] [zero-width-joiner](https://emojipedia.org/zero-width-joiner)
+- [ ] [U+FE0E and U+FE0F](https://stackoverflow.com/questions/38100329/what-does-u-ufe0f-in-an-emoji-mean-is-it-the-same-if-i-delete-it)
 
 ## Frameworks and Libraries
 [DaisyUI](https://daisyui.com/)  
 [TailwindCSS](https://tailwindcss.com/)
+
+
+## Unicode Data Files
+
+This project uses the following emoji data files from Unicode:
+
+- `emoji-test.txt` (Version 16.0, downloaded from [Unicode Emoji Test Data](https://unicode.org/Public/emoji/16.0/emoji-test.txt))
+- `emoji-sequences.txt` (Version 16.0, downloaded from [Unicode Emoji Sequences](https://unicode.org/Public/emoji/16.0/emoji-sequences.txt))
+- `emoji-zwj-sequences.txt` (Version 16.0, downloaded from [Unicode ZWJ Emoji Sequences](https://unicode.org/Public/emoji/16.0/emoji-zwj-sequences.txt))
+
+### Unicode License
+
+The Unicode Data Files used in this project are licensed under the Unicode License v3. You can find the Unicode License in the `/unicode/UNICODE_LICENSE.txt` file, or view it online at [Unicode License](https://www.unicode.org/license.html).  
+For additional information on copyright, visit [Unicode's Copyright Page](https://www.unicode.org/copyright.html).
+
+<!--
+### Acknowledgements
+
+Emoji data files are provided by [The Unicode Consortium](https://unicode.org). This project uses Unicode Data Files as permitted under the Unicode License v3. More information can be found on Unicode's [Copyright Page](https://www.unicode.org/copyright.html).
+-->
--- src/myscripts.js
+++ src/myscripts.js
@@ -13,17 +13,55 @@ themeToggle.addEventListener('change', function() {
   document.documentElement.setAttribute('data-theme', theme);
 });
 
+let zwjEmojis = [];
+
+async function loadZwjEmojis() {
+    if (zwjEmojis.length > 0) return
+
+    const response = await fetch('unicode/emoji-zwj-sequences.txt');
+    const text = await response.text();
+
+    // Filters lines that do not begin with # and extracts emoji within the brackets ()
+    const lines = text.split('\n').filter(line => !line.startsWith('#'));
+    zwjEmojis = lines.map(line => {
+        const match = line.match(/\((.*?)\)/);
+        return match ? match[1] : null;
+    }).filter(zwjEmoji => zwjEmoji !== null);
+}
+
+async function randomEmoji() {
+    await loadZwjEmojis();
+    const randomIndex = Math.floor(Math.random() * zwjEmojis.length);
+    const zwjEmoji = zwjEmojis[randomIndex];
+    document.getElementById('textInput').value = zwjEmoji;
+    copyText();
+}
+
+window.onload = loadEmojis;
+
 function copyText() {
     let inputText = document.getElementById('textInput').value;
     document.getElementById('displayText').innerText = inputText;
 
     const resultElement = document.getElementById('result');
+    const debugText = document.getElementById('debugText');
 
     let isTextEmoji = isEmoji(inputText) ? 'is' : 'is not';
     let textUniCode = getUniCode(inputText);
     let analyzedEmoji = analyzeEmoji(inputText);
 
-    resultElement.innerText = `"${inputText}" ${isTextEmoji} an emoji.\nLength: ${inputText.length}.\nUnicode: ${textUniCode}\n\nEmoji Characters\n${analyzedEmoji}`;
+    debugText.innerText = `"${inputText}" ${isTextEmoji} an emoji.\nLength: ${inputText.length}.\nUnicode: ${textUniCode}\n`;
+
+    let count = 1;
+    resultElement.innerHTML = "";
+    for (const [key, value] of Object.entries(analyzedEmoji)) {
+        resultElement.innerHTML += `<tr>
+                <th>${count}</th>
+                <th>${key}</th>
+                <td>${value}</td>
+              <tr>`
+        count++;
+    }
 
 }
 
@@ -41,13 +79,14 @@ function getUniCode(text) {
         .join(' ');
 }
 
-function analyzeEmoji(emojiText){
+function analyzeEmoji(emojiText) {
     const characters = Array.from(emojiText);
 
-    const characterInfo = characters.map(char => {
+    const characterMap = characters.reduce((map, char) => {
         const codePoint = char.codePointAt(0).toString(16).toUpperCase();
-        return `U+${codePoint.padStart(4, '0')}: ${char}`;
-    }).join('\n');
-    
-    return characterInfo;
+        map[char] = `U+${codePoint.padStart(4, '0')}`;
+        return map;
+    }, {});
+
+    return characterMap;
 }
